5c7562d23e2e2fa8d1ca9be22f54f899a561b297
vvnvvplg <vvnvvplg@gmail.com>
2019-11-23 07:26:44 +0300

    add project files

--- flask/app.py
+++ flask/app.py
@@ -1,26 +1,46 @@
-#!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_redis import FlaskRedis
+import json
 
 app = Flask(__name__)
+api = Api(app)
+redis_client = FlaskRedis(app)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
+# parser = reqparse.RequestParser()
+# parser.add_argument('links', type=list, required=True, help="Must be LINKS", location='json')
+def decode_from_bin(data):
+    return [ x.decode('utf-8') for x in data ]
 
-@app.route("/")
-def index():
-    return "Hello, world"
+
+class Home(Resource):
+    def get(self):
+        redis_data = redis_client.lrange("links", 0, -1)
+        return {"links": decode_from_bin(redis_data)}
+    def post(self):
+        links = request.json["links"]
+        try:
+            redis_client.rpush("links", *links )
+            status = "ok"
+        except Exception as error:
+            status = repr(error)
+
+        return {"status": f'{status}'}
+
+
+
+
+class VisitedLinks(Resource):
+    def post(self):
+        return {'hello': 'world'}
+
+class VisitedDomains(Resource):
+    def get(self):
+        return links
+
+api.add_resource(Home, '/')
+# api.add_resource(VisitedLinks, '/visited_links')
+# api.add_resource(VisitedDomains, '/visited_domains')
 
 if __name__ == '__main__':
     app.run(debug=True)
